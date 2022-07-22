e5480810f29f6d4a2426ad6aac3442f21c45d1ee
Amirzhan <albert.git1@gmail.com>
2022-07-22 16:21:25 +0500

    30. Обозреватель Хранилища изображений в Azure.

--- Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -22,7 +22,7 @@ public class ApplicationDbContext : DbContext
             Price = 15,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "http://127.0.0.1:10000/devstoreaccount1/mango/14.jpg",
             CategoryName = "Appetizer"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -32,7 +32,7 @@ public class ApplicationDbContext : DbContext
             Price = 13.99,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "http://127.0.0.1:10000/devstoreaccount1/mango/12.jpg",
             CategoryName = "Appetizer"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -42,7 +42,7 @@ public class ApplicationDbContext : DbContext
             Price = 10.99,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "http://127.0.0.1:10000/devstoreaccount1/mango/11.jpg",
             CategoryName = "Dessert"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -52,7 +52,7 @@ public class ApplicationDbContext : DbContext
             Price = 15,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "http://127.0.0.1:10000/devstoreaccount1/mango/13.jpg",
             CategoryName = "Entree"
         });
     }
--- Mango.Services.ProductAPI/Migrations/20220722111519_SeedProducts.Designer.cs
+++ Mango.Services.ProductAPI/Migrations/20220722111519_SeedProducts.Designer.cs
@@ -2,6 +2,7 @@
 using Mango.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220722111519_SeedProducts")]
+    partial class SeedProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.7");
