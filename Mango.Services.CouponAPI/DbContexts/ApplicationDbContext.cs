b53646d2f9371107408074dd385fbdc7b6da8449
Amirzhan <albert.git1@gmail.com>
2022-09-07 20:20:25 +0500

    94. Настройка проекта с API по управлению Купонами.

---
+++ Mango.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
