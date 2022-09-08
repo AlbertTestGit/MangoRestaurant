c33b46c8a6e4469981430b78faf37eff3b02e0a6
Amirzhan <albert.git1@gmail.com>
2022-09-08 15:12:47 +0500

    96. Репозиторий для хранения данных о Купонах.

--- Mango.Services.CouponAPI/Migrations/20220908100614_SeedCouponDatabase.Designer.cs
+++ Mango.Services.CouponAPI/Migrations/20220908100614_SeedCouponDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using Mango.Services.CouponAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220908100614_SeedCouponDatabase")]
+    partial class SeedCouponDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
