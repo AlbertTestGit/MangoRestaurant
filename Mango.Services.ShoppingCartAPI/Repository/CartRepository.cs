54433eab00b179d0c0da4c4540eac2d8e8041b86
Amirzhan <albert.git1@gmail.com>
2022-09-08 21:47:13 +0500

    99. Настройка использования Купонов в API Корзины Покупок.

--- Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -111,6 +111,26 @@ public class CartRepository : ICartRepository
         }
     }
 
+    public async Task<bool> ApplyCoupon(string userId, string couponCode)
+    {
+        var cartFromDb = await _dbContext.CartHeaders.FirstOrDefaultAsync(u => u.UserId == userId);
+        cartFromDb.CouponCode = couponCode;
+
+        _dbContext.CartHeaders.Update(cartFromDb);
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> RemoveCoupon(string userId)
+    {
+        var cartFromDb = await _dbContext.CartHeaders.FirstOrDefaultAsync(u => u.UserId == userId);
+        cartFromDb.CouponCode = "";
+
+        _dbContext.CartHeaders.Update(cartFromDb);
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }
+
     public async Task<bool> ClearCart(string userId)
     {
         var cartHeaderFromDb = await _dbContext.CartHeaders.FirstOrDefaultAsync(u =>
